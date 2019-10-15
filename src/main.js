13f353fddebef0eab2202976b37bfae42cf54f84
WENI HSU <hsu.weni@gmail.com>
2019-10-15 22:50:34 +0800

    加入 BootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueRouter);
+Vue.use(BootstrapVue);
+
 Vue.config.productionTip = false;
 
 // 引入 router 設定檔
